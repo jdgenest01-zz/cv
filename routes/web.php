3bc71d388c55f198ed8154321a8ace2b50066152
job <j.d.genest@hotmail.com>
2017-08-10 11:43:38 -0400

    test avec les vue
    ajout de fichier blade test avec les vue et le web

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,19 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return 'hello word';//view('welcome');''
-    return view('');
+Route::get('/', function()
+{
+    return View::make('pages.home');
+});
+Route::get('about', function()
+{
+    return View::make('pages.about');
+});
+Route::get('projects', function()
+{
+    return View::make('pages.projects');
+});
+Route::get('contact', function()
+{
+    return View::make('pages.contact');
 });
